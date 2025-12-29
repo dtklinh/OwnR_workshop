b3d3826782136df5eee9af0d5fba7e01fd9b0803
dtklinh <dangtruongkhanhlinh@gmail.com>
2025-12-29 23:09:09 +0100

    create a png for Kathrina

--- code/Katharina_proj/Pair_Correlation_Analysis.R
+++ code/Katharina_proj/Pair_Correlation_Analysis.R
@@ -23,13 +23,15 @@ WrenchWrapper <- function(PhyloObjct){
   return(phyloseq(otu_table(cnt_table_normalized, taxa_are_rows = T), tax_table(PhyloObjct %>% tax_table()), sample_data(PhyloObjct %>% sample_data())))
 }
 
-CRC <- readRDS("../raw_data/CRC_677_seqs_tree.rds")
+CRC <- readRDS("./data/Katharina_proj/raw_data/CRC_677_seqs_tree_v2.rds")
 
 #tax_tab <- CRC %>% tax_table() %>% as.data.frame()
 
-Species_lst <- c("Bacteroides fragilis", "Fusobacterium necrophorum", "Gemella morbillorum",
-                 "Parvimonas micra", "Peptostreptococcus stomatis", "Prevotella intermedia",
-                 "Solobacterium moorei", "Streptococcus gallolyticus", "Bifidobacterium pseudocatenulatum", "Lacticaseibacillus casei")
+# Species_lst <- c("Bacteroides fragilis", "Fusobacterium necrophorum", "Gemella morbillorum",
+#                  "Parvimonas micra", "Peptostreptococcus stomatis", "Prevotella intermedia",
+#                  "Solobacterium moorei", "Streptococcus gallolyticus", "Bifidobacterium pseudocatenulatum", "Lacticaseibacillus casei")
+Species_lst <- c("Fusobacterium nucleatum", "Gemella morbillorum", "Parvimonas micra", 
+                 "Prevotella intermedia", "Peptostreptococcus stomatis", "Solobacterium moorei")
 taxa_lst <- name2taxid(Species_lst)
 
 ##CRC_wrench <- CRC %>% WrenchWrapper()
@@ -48,12 +50,14 @@ CRC_RmLowTaxa_RelAbun <- phyloseq::transform_sample_counts(CRC_RmLowTaxa, functi
 ## calculate and plot SparCC
 cc <- sparcc(CRC_RmLowTaxa %>% otu_table() %>% t(), iter = 100)
 dat <- cc$Cor
-rownames(dat) <- CRC_RmLowTaxa %>% tax_table() %>% as.data.frame() %>%  select(Species) %>% unlist()
+rownames(dat) <- CRC_RmLowTaxa %>% tax_table() %>% as.data.frame() %>%  select(species) %>% unlist()
 colnames(dat) <- rownames(dat)
 
 png("SparCC.png")
 pheatmap(dat, display_numbers = T)
 dev.off()
+
+ggplot2::ggsave("./results/Katharina_proj/SparCC_Dec2025.png", pheatmap(dat, display_numbers = T))
 
 ## test
 tmp_tab <- CRC_wrench %>% otu_table() %>% as.data.frame()
--- requirements.R
+++ requirements.R
@@ -1,6 +1,6 @@
 ## if nesscessary, install libglpk-dev, libgsl-dev, libfl-dev
 ## sudo apt install libcairo2-dev libfontconfig1-dev pandoc
-if (!requireNamespace("BiocManager", quietly = TRUE)) install.packages("BiocManager")
+##if (!requireNamespace("BiocManager", quietly = TRUE)) install.packages("BiocManager")
 #BiocManager::install(c("phyloseq", "microbiome", "ComplexHeatmap"), update = FALSE)
 
 # install.packages(
@@ -10,9 +10,14 @@ if (!requireNamespace("BiocManager", quietly = TRUE)) install.packages("BiocMana
 
 # requirements.R
 required_pkgs <- c(
+  "phyloseq",
   "tidyverse",
   "data.table",
-  "readxl"
+  "readxl",
+  "SpiecEasi",
+  "DESeq2",
+  "Wrench",
+  "devtools"
 )
 
 to_install <- setdiff(required_pkgs, rownames(installed.packages()))
